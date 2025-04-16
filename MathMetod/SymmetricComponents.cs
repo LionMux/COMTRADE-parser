a9437787da9c2fbb4f341d26bfbf434e2ab8aac6
Лев Мухачев <muhachevlev@gmail.com>
2025-04-16 13:31:13 +0300

    refactor

--- MathMetod/SymmetricComponents.cs
+++ MathMetod/SymmetricComponents.cs
@@ -7,51 +7,53 @@ namespace COMTRADE_parser.MathMetod
         private readonly List<AnalogChannelConfig> _cfgChannels;
         private readonly List<List<double>> _datSamples;
         private List<int> _phaseIndices = new List<int>();
+        List<double> phaseA = new List<double>();
+        List<double> phaseB = new List<double>();
+        List<double> phaseC = new List<double>();
 
         public SymmetricComponents(List<AnalogChannelConfig> cfgChannels, List<List<double>> datSamples)
         {
             _cfgChannels = cfgChannels;
             _datSamples = datSamples;
-
-            // Находим индексы фаз A, B, C
-            FindPhaseIndices();
         }
         public List<List<double>> CalculateI0()
         {
             List<List<double>> I0 = new List<List<double>>();
-
-            foreach (var sample in _datSamples)
+            for (int i = 0; i < _cfgChannels.Count; i++)
             {
-                double sum = sample[_phaseIndices[0]] + sample[_phaseIndices[1]] + sample[_phaseIndices[2]];
+                int j = i;
+                FindPhaseIndices()
+                double sum = phaseA[i] + phaseB[i] + phaseC[i];
                 I0.Add(sum / 3.0);
             }
+
 
             return I0;
         }
         private void FindPhaseIndices()
         {
             _phaseIndices.Clear();
-
+            var channel = _cfgChannels[j];
+            // Проверяем, является ли канал фазой A, B или C (пример для токов)
             for (int i = 0; i < _cfgChannels.Count; i++)
             {
-                var channel = _cfgChannels[i];
-                // Проверяем, является ли канал фазой A, B или C (пример для токов)
-                if (channel.Name.ToUpper().Contains("IA"))
+                foreach (var sample in _datSamples)
                 {
-                    _phaseIndices.Add(i);
-                }
-                else if (channel.Name.ToUpper().Contains("IB"))
-                {
-                    _phaseIndices.Add(i);
-                }
-                else if (channel.Name.ToUpper().Contains("IC"))
-                {
-                    _phaseIndices.Add(i);
+                    if (channel.Name.ToUpper().Contains("IA"))
+                    {
+                        phaseA.Add(sample[i]);
+
+                    }
+                    else if (channel.Name.ToUpper().Contains("IB"))
+                    {
+                        phaseB.Add(sample[i]);
+                    }
+                    else if (channel.Name.ToUpper().Contains("IC"))
+                    {
+                        phaseC.Add(sample[i]);
+                    }
                 }
             }
-
-            if (_phaseIndices.Count != 3)
-                throw new InvalidDataException("Не найдены фазы A, B, C.");
         }
 
     }
