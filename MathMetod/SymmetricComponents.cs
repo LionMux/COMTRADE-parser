6ecdb9f11887ce981ad2bd9dff950916e99d8b15
Лев Мухачев <muhachevlev@gmail.com>
2025-04-17 11:51:27 +0300

    diplom

--- MathMetod/SymmetricComponents.cs
+++ MathMetod/SymmetricComponents.cs
@@ -1,60 +1,64 @@
-﻿
-namespace COMTRADE_parser.MathMetod
+﻿namespace COMTRADE_parser.MathMetod
 {
-
     internal class SymmetricComponents
     {
         private readonly List<AnalogChannelConfig> _cfgChannels;
         private readonly List<List<double>> _datSamples;
-        private List<int> _phaseIndices = new List<int>();
-        List<double> phaseA = new List<double>();
-        List<double> phaseB = new List<double>();
-        List<double> phaseC = new List<double>();
+        private List<int> _phaseIndicesA = new List<int>();
+        private List<int> _phaseIndicesB = new List<int>();
+        private List<int> _phaseIndicesC = new List<int>();
 
         public SymmetricComponents(List<AnalogChannelConfig> cfgChannels, List<List<double>> datSamples)
         {
             _cfgChannels = cfgChannels;
             _datSamples = datSamples;
+            FindPhaseIndices();
         }
+
         public List<List<double>> CalculateI0()
         {
             List<List<double>> I0 = new List<List<double>>();
-            for (int i = 0; i < _cfgChannels.Count; i++)
+            if (_phaseIndicesA.Count != _phaseIndicesB.Count || _phaseIndicesA.Count != _phaseIndicesB.Count)
             {
-                int j = i;
-                FindPhaseIndices()
-                double sum = phaseA[i] + phaseB[i] + phaseC[i];
-                I0.Add(sum / 3.0);
+                throw new ArgumentException("Недостаточно значений для расчета I0");
             }
-
+            // Проходим по всем временным точкам (сэмплам)
+            for (int i = 0; i < _phaseIndicesA.Count; i++)
+            {
+                double sum = 0;
+                List<double> sampleI0 = new List<double>();
+                int sampleA = _phaseIndicesA[i];
+                int sampleB = _phaseIndicesB[i];
+                int sampleC = _phaseIndicesC[i];
+                foreach (var sample in _datSamples)
+                {
+                    sum = sample[sampleA] + sample[sampleB] + sample[sampleC];
+                    sampleI0.Add(sum / 3.0);
+                }
+                I0.Add(sampleI0);
+            }
 
             return I0;
         }
+
         private void FindPhaseIndices()
         {
-            _phaseIndices.Clear();
-            var channel = _cfgChannels[j];
-            // Проверяем, является ли канал фазой A, B или C (пример для токов)
             for (int i = 0; i < _cfgChannels.Count; i++)
             {
-                foreach (var sample in _datSamples)
+                var channel = _cfgChannels[i];
+                if (channel.Name.ToUpper().Contains("IA"))
                 {
-                    if (channel.Name.ToUpper().Contains("IA"))
-                    {
-                        phaseA.Add(sample[i]);
-
-                    }
-                    else if (channel.Name.ToUpper().Contains("IB"))
-                    {
-                        phaseB.Add(sample[i]);
-                    }
-                    else if (channel.Name.ToUpper().Contains("IC"))
-                    {
-                        phaseC.Add(sample[i]);
-                    }
+                    _phaseIndicesA.Add(i);
+                }
+                else if (channel.Name.ToUpper().Contains("IB"))
+                {
+                    _phaseIndicesB.Add(i);
+                }
+                else if (channel.Name.ToUpper().Contains("IC"))
+                {
+                    _phaseIndicesC.Add(i);
                 }
             }
         }
-
     }
 }
